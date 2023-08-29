31f1a26385d00a67e1e67c49ea66338aa0a4e046
Asilbek Turgunboev <asilbekturgunboev@Asilbeks-MacBook-Pro.local>
2023-08-30 01:48:19 +0500

    want to merge with updated calalog service

--- auth_service/auth_app/database.py
+++ auth_service/auth_app/database.py
@@ -1,23 +1,37 @@
 # auth_app/database.py
 
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# import os
+
+# POSTGRES_USER = os.getenv("POSTGRES_USER")
+# POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres-service")
+# POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+# POSTGRES_DB = os.getenv("POSTGRES_DB", "panda_auth")
+
+# # Construct the DATABASE_URL
+# DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import os
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres-service")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "panda_auth")
+# DATABASE_URL = os.environ.get("DB_CONNECTION_STRING")
+DATABASE_URL = "postgresql://asilbek:Asilbek2001@localhost/panda_auth"
 
-# Construct the DATABASE_URL
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# Create a synchronous engine
+engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=30)
 
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+# Create a session maker class bound to this engine
+SessionLocal = sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
+)
 Base = declarative_base()
 
 
