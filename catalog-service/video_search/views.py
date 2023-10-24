6587a73401b02d0f79ace81f178add970578eebe
Asilbek Turgunboev <asilbekturgunboev@Asilbeks-MacBook-Pro.local>
2023-10-25 01:18:45 +0500

    updated search

--- catalog-service/video_search/views.py
+++ catalog-service/video_search/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.db.models import Q
-from video_app.models import Movie, Series
+from video_app.models import Movie, Series, Director, Genre
 from .utils import standardResponse
 from rest_framework.views import APIView
 
@@ -8,45 +8,78 @@ from rest_framework.views import APIView
 class GeneralSearch(APIView):
     def get(self, request):
         query = request.GET.get('q', '')
-        print(query)
+
         movies = Movie.objects.filter(
             Q(title__icontains=query) |
-            Q(genre__name__icontains=query)
+            Q(genre__name__icontains=query) |
+            Q(director__name__icontains=query)
         ).distinct()
 
         series = Series.objects.filter(
             Q(title__icontains=query) |
-            Q(genre__name__icontains=query)
+            Q(genre__name__icontains=query) |
+            Q(director__name__icontains=query)
         ).distinct()
 
-        movies_list = [movie.title for movie in movies]
-        series_list = [serie.title for serie in series]
+        # Serialize the data
+        movies_list = [{
+            "id": movie.id,
+            "title": movie.title,
+            "release_date": movie.release_date,
+            "genre": [genre.name for genre in movie.genre.all()],
+            "director": movie.director.name,
+            "thumbnail": movie.thumbnail_image.url if movie.thumbnail_image else None,
+            "rating": movie.rating
+        } for movie in movies]
+
+        series_list = [{
+            "id": serie.id,
+            "title": serie.title,
+            "release_date": serie.release_date,
+            "genre": [genre.name for genre in serie.genre.all()],
+            "director": serie.director.name,
+            "thumbnail": serie.thumbnail_image.url if serie.thumbnail_image else None,
+            "rating": serie.rating
+        } for serie in series]
+
         data = {"movies": movies_list, "series": series_list}
 
-        return standardResponse(status="success", message="Data fetched successfully", data=data)
+        return JsonResponse({"status": "success", "message": "Data fetched successfully", "data": data})
 
 
 class GenreSearch(APIView):
     def get(self, request):
-        # Get the 'genres' query parameter and split it into a list
         genres = request.GET.get('genres', '').split(',')
-
-        # Remove any leading/trailing whitespaces from each genre
         genres = [genre.strip() for genre in genres]
 
-        # Create a Q object for OR-ing the filters together
         query = Q(genre__name__icontains=genres[0])
         for genre in genres[1:]:
             query |= Q(genre__name__icontains=genre)
 
-        # Search in the Movie and Series models by genre
         movies = Movie.objects.filter(query).distinct()
         series = Series.objects.filter(query).distinct()
 
-        # Serialize the data (for demonstration, we'll only send the titles)
-        movies_list = [movie.title for movie in movies]
-        series_list = [serie.title for serie in series]
+        # Serialize the data (similar to the GeneralSearch class)
+        movies_list = [{
+            "id": movie.id,
+            "title": movie.title,
+            "release_date": movie.release_date,
+            "genre": [genre.name for genre in movie.genre.all()],
+            "director": movie.director.name,
+            "thumbnail": movie.thumbnail_image.url if movie.thumbnail_image else None,
+            "rating": movie.rating
+        } for movie in movies]
+
+        series_list = [{
+            "id": serie.id,
+            "title": serie.title,
+            "release_date": serie.release_date,
+            "genre": [genre.name for genre in serie.genre.all()],
+            "director": serie.director.name,
+            "thumbnail": serie.thumbnail_image.url if serie.thumbnail_image else None,
+            "rating": serie.rating
+        } for serie in series]
 
         data = {"movies": movies_list, "series": series_list}
 
-        return standardResponse(status="success", message="Data fetched successfully", data=data)
+        return JsonResponse({"status": "success", "message": "Data fetched successfully", "data": data})
