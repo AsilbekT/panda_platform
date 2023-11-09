35d2236678286435d2e6900d58e434cca8009832
Asilbek Turgunboev <asilbekturgunboev@Asilbeks-MacBook-Pro.local>
2023-11-10 00:30:44 +0500

    updated the url for click

--- billing_service/subscription_plans/urls.py
+++ billing_service/subscription_plans/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('plans/', views.SubscriptionPlanListView.as_view(), name='list-plans'),
+    path('payment-url/', views.PaymentUrlView.as_view(), name='create-payment-url'),
     path('subscribe/', views.SubscribeView.as_view(), name='subscribe'),
     path('<str:username>/subscriptions/',
          views.UserSubscriptionView.as_view(), name='subscriptions'),
