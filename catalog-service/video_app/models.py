7d7f9471fbf35ab31e3d3e21d481d7ddd452b6bf
Asilbek Turgunboev <asilbekturgunboev@Asilbeks-MacBook-Pro.local>
2023-11-10 23:13:05 +0500

    updated the description

--- catalog-service/video_app/models.py
+++ catalog-service/video_app/models.py
@@ -10,6 +10,7 @@ from django.utils.text import slugify
 
 class SubscriptionPlan(models.Model):
     name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration_days = models.IntegerField()
 
