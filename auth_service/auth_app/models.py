9f093b836687c54513383179e14e05125dcdb292
Asilbek Turgunboev <asilbekturgunboev@Asilbeks-MacBook-Pro.local>
2023-12-15 01:11:29 +0500

    updated the session bugs

--- auth_service/auth_app/models.py
+++ auth_service/auth_app/models.py
@@ -8,13 +8,14 @@ Base = declarative_base()
 class UserSession(Base):
     __tablename__ = 'user_sessions'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    username = Column(String, ForeignKey('users.username'))
     # Stores the device information as a text string
     device_info = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
     user = relationship("User", back_populates="sessions")
     logged_out = Column(Boolean, default=False)
     ip_address = Column(String, nullable=True)
+    token = Column(String)
 
     def logout(self):
         self.logged_out = True
@@ -23,7 +24,7 @@ class UserSession(Base):
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
+    username = Column(String, unique=True, nullable=False)
     phone_number = Column(String)
     password_hash = Column(String)
     creation_date = Column(DateTime, default=datetime.utcnow)
@@ -32,6 +33,6 @@ class User(Base):
 
     def get_active_sessions(self, db: UserSession):
         return db.query(UserSession).filter(
-            UserSession.user_id == self.id,
+            UserSession.username == self.username,
             UserSession.logged_out == False
         ).all()
--- auth_service/auth_app/views.py
+++ auth_service/auth_app/views.py
@@ -1,11 +1,12 @@
-from fastapi import HTTPException, Header, Depends, Request
+from fastapi import HTTPException, Header, Depends, Request, Query
 from .models import User, UserSession
 from .utils import (
     hash_password,
     create_jwt_token,
     verify_jwt_token,
     verify_password,
-    token_blacklist  # Import the token_blacklist
+    invalidate_token,
+    redis_client
 )
 from .database import get_db
 from sqlalchemy.orm import Session
@@ -28,6 +29,10 @@ class UserLogin(BaseModel):
     password: str
 
 
+class LogoutSessionRequest(BaseModel):
+    session_id: int
+
+
 async def register(user: UserCreate, db: Session = Depends(get_db)) -> StandardResponse:
     # Check if the user with the given username or phone number already exists
     existing_user = db.query(User).filter(
@@ -46,7 +51,7 @@ async def register(user: UserCreate, db: Session = Depends(get_db)) -> StandardR
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    url = "http://127.0.0.1:7008/users"
+    url = "http://127.0.0.1:8000/users"
 
     # Username to send (assuming user.username is defined elsewhere in your code)
     data = {
@@ -64,6 +69,7 @@ async def register(user: UserCreate, db: Session = Depends(get_db)) -> StandardR
 
 
 async def login(request: Request, user: UserLogin, db: Session = Depends(get_db)) -> StandardResponse:
+    client_host = request.client.host
     db_user = db.query(User).filter(User.username == user.username).first()
     if not db_user or not verify_password(user.password, db_user.password_hash):
         raise HTTPException(status_code=400, detail="Invalid credentials")
@@ -72,10 +78,10 @@ async def login(request: Request, user: UserLogin, db: Session = Depends(get_db)
     request_data = await request.json()
     device_info = request_data.get('device_info')
     client_host = request.client.host
-    print(request)
+
     # Check and manage the number of active sessions
     active_sessions = db.query(UserSession).filter(
-        UserSession.user_id == db_user.id,
+        UserSession.username == db_user.username,
         UserSession.logged_out == False
     ).order_by(UserSession.created_at.asc()).all()
 
@@ -84,19 +90,19 @@ async def login(request: Request, user: UserLogin, db: Session = Depends(get_db)
         oldest_session = active_sessions[0]
         db.delete(oldest_session)
 
-    # Create a new session
-    new_session = UserSession(
-        user_id=db_user.id,
-        device_info=device_info,
-        ip_address=client_host
-    )
-    db.add(new_session)
-
-    # Create JWT token
+    # Create JWT token first before creating a new session
     token_data = {"sub": db_user.id, "username": db_user.username}
     token = create_jwt_token(token_data)
 
-    db.commit()
+    # Create a new session
+    new_session = UserSession(
+        username=db_user.username,
+        device_info=device_info,
+        ip_address=client_host,
+        token=token
+    )
+    db.add(new_session)
+    db.commit()  # Commit the transaction to save the session to the database
 
     return StandardResponse(
         status="success",
@@ -131,21 +137,22 @@ async def logout(token: str = Depends(get_token), db: Session = Depends(get_db))
 
     # Delete the user's session
     user_session = db.query(UserSession).filter(
-        UserSession.user_id == user.id
+        UserSession.username == user.username
     ).first()
     if user_session:
         db.delete(user_session)
-        token_blacklist.add(token)
+        invalidate_token(token)
         db.commit()
 
     return StandardResponse(status="success", message="Successfully logged out")
 
 
 async def verify_token(token: str = Depends(get_token)) -> StandardResponse:
-    if token in token_blacklist:  # Check if the token is in the blacklist
+    if redis_client.get(token):
         raise HTTPException(
-            status_code=401, detail="Token has been invalidated")
-
+            status_code=401,
+            detail="Access denied. The provided token is no longer valid and cannot be used for authentication."
+        )
     try:
         payload = verify_jwt_token(token)
         return StandardResponse(status="success", message="Token is valid", data={"username": payload.get("username")})
@@ -155,20 +162,55 @@ async def verify_token(token: str = Depends(get_token)) -> StandardResponse:
         raise HTTPException(status_code=401, detail="Invalid token")
 
 
-async def get_active_sessions(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(User).get(user_id)
+async def get_active_sessions(token: str = Depends(get_token), username: str = Query(...), db: Session = Depends(get_db)) -> StandardResponse:
+    # Retrieve the user based on username
+    if redis_client.get(token):
+        raise HTTPException(
+            status_code=401,
+            detail="Access denied. The provided token is no longer valid."
+        )
+
+    user = db.query(User).filter(User.username == username).first()
+
+    # Check if the user exists
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        return StandardResponse(status="error", message="User not found")
 
+    # Retrieve active sessions for the user
     sessions = user.get_active_sessions(db)
-    return sessions  # Modify this return statement as per your response schema
+    for i in sessions:
+        print(i.token)
+    # Check if there are no active sessions
+    if not sessions:
+        return StandardResponse(status="error", message="No active sessions found for the user")
+
+    # Format the sessions data for the response
+    sessions_data = [{"session_id": session.id, "device_info": session.device_info,
+                      "created_at": session.created_at.isoformat()} for session in sessions]
+
+    return StandardResponse(status="success", message="Active sessions retrieved successfully", data=sessions_data)
 
 
-async def logout_session(session_id: int, db: Session = Depends(get_db)):
-    session = db.query(UserSession).get(session_id)
+async def logout_session(request: LogoutSessionRequest, token: str = Depends(get_token), db: Session = Depends(get_db)) -> StandardResponse:
+    if redis_client.get(token):
+        raise HTTPException(
+            status_code=401,
+            detail="Access denied. The provided token is no longer valid."
+        )
+
+    session_id = request.session_id
+
+    # Find the session by ID
+    session = db.query(UserSession).filter(
+        UserSession.id == session_id).first()
     if not session:
         raise HTTPException(status_code=404, detail="Session not found")
 
+    # Invalidate the token of the session
+    invalidate_token(session.token)
+
+    # # Update the session as logged out
     session.logout()
     db.commit()
-    return {"message": "Logged out from the session successfully"}
+
+    return StandardResponse(status="success", message="Logged out from the session successfully", data={})
